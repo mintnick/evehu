7651abc997212c86e3b0913c5158360a50bf4091
Ke Ning <mintnick@gmail.com>
2022-08-21 03:44:39 +0800

    add toast. update tables on home page

--- controllers/home.js
+++ controllers/home.js
@@ -13,24 +13,11 @@ const titles = [
     'moving_characters',
 ]
 
-const date_titles = ['birthday', 'end_date', 'start_date', 'date_founded']
-
 module.exports = async function(req, res) {
     const data = {};
     for (const title of titles) {
         const value = await JSON.parse(await res.app.redis.get(title));
-        if (value) {
-            for (const i of value) 
-                if(i.birthday) {
-                    const array = i.birthday.slice(0, 10).split('-')
-                        year = array[0], month = parseInt(array[1]), day = array[2],
-                        time = i.birthday.slice(11, 19);
-                    i.birthday = `${year}年${month}月${day}日 ${time}`;
-                }
-        }
-
-        
-        data[title] = value;
+        if(value) data[title] = value;
     }
     data.title = 'Home';
     return data;
--- tasks/update_redis_home.js
+++ tasks/update_redis_home.js
@@ -3,15 +3,15 @@ module.exports = async function (app) {
     setRedis(app, 'large_alliances', 'select alliance_id id, name, member_count, diff_7days from alliances order by member_count desc limit 10');
     setRedis(app, 'growing_alliances', 'select alliance_id id, name, member_count, diff_7days from alliances where diff_7days IS NOT NULL order by diff_7days desc limit 10');
     setRedis(app, 'shrinking_alliances', 'select alliance_id id, name, member_count, diff_7days from alliances where diff_7days IS NOT NULL order by diff_7days limit 10');
-    setRedis(app, 'new_alliances', 'select alliance_id id, name, date_founded birthday, member_count, diff_7days from alliances  order by date_founded desc limit 10');
+    setRedis(app, 'new_alliances', 'select alliance_id id, name, date_format(date_founded, "%Y年%m月%d日 %H:%i") birthday, member_count from alliances  order by date_founded desc limit 10');
 
-    setRedis(app, 'large_corporations', 'select corporation_id id, alliance_id, name, member_count, diff_7days from corporations where corporation_id > 98000000 order by member_count desc limit 10');
-    setRedis(app, 'growing_corporations', 'select corporation_id id, alliance_id, name, member_count, diff_7days from corporations where diff_7days IS NOT NULL and corporation_id > 98000000 order by diff_7days desc limit 10');
-    setRedis(app, 'shrinking_corporations', 'select corporation_id id, alliance_id, name, member_count, diff_7days from corporations where diff_7days IS NOT NULL and corporation_id > 98000000 order by diff_7days limit 10');
-    setRedis(app, 'new_corporations', 'select corporation_id id, alliance_id, date_founded birthday, name, member_count, diff_7days from corporations order by date_founded desc limit 10');
+    setRedis(app, 'large_corporations', 'select corporation_id id, name, member_count, diff_7days from corporations where corporation_id > 98000000 order by member_count desc limit 10');
+    setRedis(app, 'growing_corporations', 'select corporation_id id, name, member_count, diff_7days from corporations where diff_7days IS NOT NULL and corporation_id > 98000000 order by diff_7days desc limit 10');
+    setRedis(app, 'shrinking_corporations', 'select corporation_id id, name, member_count, diff_7days from corporations where diff_7days IS NOT NULL and corporation_id > 98000000 order by diff_7days limit 10');
+    setRedis(app, 'new_corporations', 'select corporation_id id, name, date_format(date_founded, "%Y年%m月%d日 %H:%i") birthday, member_count from corporations order by date_founded desc limit 10');
 
-    setRedis(app, 'new_characters', 'select character_id id, alliance_id, corporation_id, name, birthday from characters order by birthday desc limit 10');
-    setRedis(app, 'moving_characters', 'SELECT h.record_id id, h.character_id ch_id, ch.name ch_name, c.corporation_id c_id, c.name c_name, h.end_date end_date FROM `corporation_history` h ' +
+    setRedis(app, 'new_characters', 'select character_id id, name, date_format(birthday, "%Y年%m月%d日 %H:%i") birthday from characters order by birthday desc limit 10');
+    setRedis(app, 'moving_characters', 'SELECT h.record_id id, h.character_id ch_id, ch.name ch_name, c.corporation_id c_id, c.name c_name, date_format(h.start_date, "%Y年%m月%d日 %H:%i") start_date FROM `corporation_history` h ' +
                                         'left join characters ch on h.character_id = ch.character_id ' +
                                         'left join corporations c on h.corporation_id = c.corporation_id ' +
                                         'where ch.birthday < (NOW() - INTERVAL 1 DAY) ' +
